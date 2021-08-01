1cf23c5e74517787097648f3a02e92537634c01b
swapnil shukla <swapnil.shukla1508@gmail.com>
2021-08-01 09:28:03 +0530

    last commit

--- src/project1/myclass.java
+++ src/project1/myclass.java
@@ -3,6 +3,7 @@ package project1;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -11,16 +12,20 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class myclass {
-
+	 static Map <String,ArrayList<Integer>> hashmap = new HashMap<String,ArrayList<Integer>>();
 	public static void main(String[] args) {
 	
       Map <String,Integer> dict= new HashMap<String,Integer>();
       Map <String,Integer> dict1 = new HashMap<String,Integer>();
+     
       Scanner sc = new Scanner(System.in);
       System.out.println("How many entries");
+      
+     
       int m = sc.nextInt();
       int value =0;
       String v ="";
+      String m1 ="";
       for(int i=0; i<m; i++)
       {
     	  String key = sc.next();
@@ -31,39 +36,63 @@ public class myclass {
      {
     	   
            v = entry.getKey();
-           int val = entry.getValue();
+          
            try {
-			String m1 =datetoday(v);
-		       Iterator<Entry<String, Integer>>iterator = dict1.entrySet().iterator();
+			 m1 =datetoday(v);
+		     /*  
+		      
 		       while (iterator.hasNext()) {
 		    	   
 	                // Get the entry at this iteration
-	                Map.Entry<String,Integer> entry1 = iterator.next();
+	              
 	  
-	                // Check if this key is the required key
-	                if (entry1.getKey() == m1) {
+	                System.out.println(entry1.getKey());
+	                if (entry1.getKey().equals(m1)) {
 	                    val = val+entry.getValue();
 	                    
 	                }
 	                
 	               
-	            }
-		       dict1.put(m1,val);
+	            }*/
+		     
+		       addValues(m1,entry.getValue());
+		     /*  if(dict1.containsKey(m1))
+		    	{
+		    	   val = val + entry.getValue();
+		    	}
+		       dict1.put(m1,val);*/
 			    
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+           
         
      
 	}
+     Iterator it = hashmap.keySet().iterator();
+     ArrayList<Integer> templist = null;
+     
+    // Map.Entry<String, ArrayList<Integer>> entry =  (Entry<String, ArrayList<Integer>>) hashmap.entrySet();
+     while(it.hasNext())
+     {
+    	 int f =0;
+    	 String key = it.next().toString();
+    	 templist = hashmap.get(key);
+    	 if(templist !=null) {
+    		 for(int h:templist) {
+    			 f += h;
+    		 }
+    	 }
+    	 
+    	 dict1.put(key,f);
+     }
      for(Entry<String,Integer> entry1 :dict1.entrySet())
      {
     	System.out.println(entry1.getKey()+entry1.getValue());
      }
      
-	   
-      
+	
       
 	}
 	public static String datetoday(String v) throws ParseException {
@@ -75,5 +104,21 @@ public class myclass {
   		return dayWeekText;
 		
 	}
-
+	 private static void addValues(String key,int n)
+     {
+     	ArrayList<Integer> templist =null;
+     	if(hashmap.containsKey(key))
+     	{
+     		templist = hashmap.get(key);
+     		if(templist == null)
+     			templist = new ArrayList<Integer>();
+     		templist.add(n);
+     	}else
+     	{
+     		templist = new ArrayList<Integer>();
+     		templist.add(n);
+     	}
+     	hashmap.put(key,templist);
+     } 
+   
 }
